b9c8a1025f1d529de5adef078bff49c43b3d8631
anipahannifah24@gmail.com <anipahannifah24@gmail.com>
2021-03-10 13:34:35 +0700

    Menambahkan gambar pada list

--- app/src/main/java/org/d3if3024/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if3024/galerihewan/Hewan.kt
@@ -1,6 +1,7 @@
 package org.d3if3024.galerihewan
 
 data class Hewan(
-        val nama : String,
-        val namaLatin : String
+        val nama: String,
+        val namaLatin:String,
+        val imageResId: Int
 )
